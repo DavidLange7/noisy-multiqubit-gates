fixed the matrix multiplication in the determinstic part of the numerical noisy-gate + some minor changes

noisy_rydberg_singlequbit_analytic.py
@@ -5,6 +5,7 @@ Created on Mon Jul 17 12:31:26 2023
 
 @author: david
 """
+#%%
 
 import numpy as np
 import scipy
@@ -53,7 +54,7 @@ def tqdm_joblib(tqdm_object):
         joblib.parallel.BatchCompletionCallBack = old_batch_callback
         tqdm_object.close()
         
-#%%
+
 class single_noisy_gate():
     def __init__(self, theta, phi, K_array, gamma):
         '''
@@ -264,14 +265,9 @@ class single_noisy_gate():
                 else:
                     res = np.matmul(U_array[-i-1], res)
                     tmp = (np.outer(np.conj(res), res))
-                    results_st1[i][j] = np.real(tmp[0][0])
-                    
-                if i == int(N/2) and j == int(shots/2):
-                    print('Halfwaypoint of simulation reached at ', datetime.datetime.now())
-            
+                    results_st1[i][j] = np.real(tmp[0][0])            
         
         results_st2 = np.array([np.sum(results_st1[k])/shots for k in range(N)])
-        plt.plot(results_st2)
         
         return(results_st2)
     

noisygate_rydberg_numerical.py
@@ -104,25 +104,25 @@ class rydberg_noisy_gate():
     def __det_part_r(self, ham):
 
         L = self.K_array
-        
-        val, vec = np.linalg.eig(-1J*ham)
-        v_m1 = sp.Matrix((np.linalg.inv(vec)))
-        vec = sp.Matrix(vec)
+
+        val, vec = np.linalg.eig(ham)
+        v_m1 = (np.linalg.inv(vec))
+
         t = sp.symbols('t', real = True)
 
-        expr1 = [sp.exp(1J*val[i]*t) for i in range(len(val))]
+        expr1 = [sp.exp(np.conj(-1J*val[i]*t)) for i in range(len(val))]
         expr2 = [sp.exp(-1J*val[i]*t) for i in range(len(val))]
 
-        expr1 = sp.diag(*expr1)
-        expr2 = sp.diag(*expr2)
+        expr1 = np.diag(expr1)
+        expr2 = np.diag(expr2)
+
 
        
         lam = []
     
         for ind in range(len(L)):
-            L_int = sp.simplify( sp.conjugate(v_m1) @ expr1 @ sp.conjugate(vec) @ sp.Matrix(L[ind]) @ vec @ expr2 @ v_m1 )
-            
-            tmp1 = sp.Matrix(-1/2*self.gamma[ind]*(dgr(L_int) @ L_int - L_int @ L_int))
+            L_int =  np.conj(v_m1).T @ expr1 @ np.conj(vec).T @ L[ind] @ vec @ expr2 @ v_m1 
+            tmp1 = sp.simplify(sp.Matrix(-1/2*self.gamma[ind]*(np.conj(L_int).T @ L_int - L_int @ L_int)))
             
             tmp2 = sp.integrate(tmp1, (t, 0, 1))
             tmp3 = sp.lambdify(t, tmp2, "numpy")
@@ -175,19 +175,18 @@ class rydberg_noisy_gate():
         
         L = self.K_array
         
-        val, vec = np.linalg.eig(-1J*ham)
+        val, vec = np.linalg.eig(ham)
         v_m1 = np.linalg.inv(vec)
         t = sp.symbols('t', real = True)
 
-        expr1 = [sp.exp(1J*val[i]*t) for i in range(len(val))]
+        expr1 = [sp.exp(np.conj(-1J*val[i]*t)) for i in range(len(val))]
         expr2 = [sp.exp(-1J*val[i]*t) for i in range(len(val))]
 
         expr1 = np.diag(expr1)
         expr2 = np.diag(expr2)
 
         for ind in range(len(L)):
-    
-            tot = sp.simplify( np.conj(v_m1) @ expr1 @ np.conj(vec) @ L[ind] @ vec @ expr2 @ v_m1 )
+            tot =  sp.simplify(np.conj(v_m1).T @ expr1 @ np.conj(vec).T @ L[ind] @ vec @ expr2 @ v_m1 )
         
             mat = sp.flatten(tot)
     
@@ -252,7 +251,7 @@ class rydberg_noisy_gate():
             
     def __modify_gate(self, U, det_part, corr_rs = None, corr_ims = None):
                                                    
-        result = U @ scipy.linalg.expm(self.__stoch_part(corr_rs, corr_ims)) @ scipy.linalg.expm(det_part)
+        result = U @ scipy.linalg.expm(det_part) @ scipy.linalg.expm(self.__stoch_part(corr_rs, corr_ims))
 
         return result
     

run.py
@@ -21,9 +21,9 @@ calling noisygate_rydberg_numerical
 
 psi_0 = np.zeros([4])
 psi_0[0] = 1
-N = 4000
-shots = 30
-t1 = 4 #amplitude damping #4s is in thesis
+N = 3000
+shots = 1
+t1 = 0.04 #amplitude damping #4s is in thesis
 
 t2 = 300*10**(-3) #dephasing damping
 
@@ -63,7 +63,7 @@ tst = ng.rydberg_noisy_gate(K_array, o, d, 1, gamma)
 results = tst.singlequbit_sample_runs(psi_0, N, shots)
 
 plt.plot(results, color = 'tab:red', label = 'noisygate')
-plt.axvline(x=t1/tg, label='$T_{dp}$', color = 'black', linestyle='dashed', alpha = 0.7)
+#plt.axvline(x=t1/tg, label='$T_{dp}$', color = 'black', linestyle='dashed', alpha = 0.7)
 # %%
 '''
 In this cell, we run the single qubit rydberg noisy gate in its analytical form,
@@ -74,7 +74,7 @@ calling noisy_rydberg_singlequbit_analytic
 '''
 
 
-t1 = 4 #amplitude damping
+t1 = 0.04 #amplitude damping
 t2 = 300*10**(-3) #dephasing
 
 o_real = 2*np.pi*10*10**(3)
@@ -85,7 +85,7 @@ tg = np.pi/o_real
 g_1 = tg/t1
 gd = tg/t2
 
-gamma = [g_1, gd]
+gamma = [g_1, g_1]
 
 K_array = [
         [sp.Matrix(([0, 0, 0, 0],
@@ -102,10 +102,10 @@ tst = nrsa.single_noisy_gate(np.pi, 0, K_array, gamma)
 psi_0 = np.zeros([4])
 psi_0[0] = 1
 N = 3000
-shots = 20
+shots = 1
 
 o = np.pi
-d = 0.001 #d = 0 causes overflow warnings, might want to add an exception
+d = 0.00001 #d = 0 causes overflow warnings, might want to add an exception
 o_p = np.sqrt(o**2 + d**2)
 
 results = tst.singlequbit_sample_runs(psi_0, N, shots, params = [1, o_p, d, o, 1, 1] ) #t, o_p, d, o, x1, x2
@@ -114,7 +114,7 @@ plt.ylabel(r"$\rho_{00}$")
 plt.xlabel(r'time in [$t_g$]')
 
 #plt.axvline(x=t1/tg, label='$T_a$', color = 'orange', linestyle='dashed', alpha = 0.5)
-plt.axvline(x=t2/tg, label='$T_{dp}$', color = 'black', linestyle='dashed', alpha = 0.7)
+#plt.axvline(x=t2/tg, label='$T_{dp}$', color = 'black', linestyle='dashed', alpha = 0.7)
 plt.plot(results[0], color = 'tab:red', label = 'noisygate')
 
 plt.legend()