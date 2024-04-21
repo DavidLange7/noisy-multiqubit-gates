fixed the stochastic part of numerical, note that in analytic there is a problem with doing with/without dephasing term. Including dephasing now works for both analtic and numerical for single qubit

noisygate_rydberg_numerical.py
@@ -1,3 +1,4 @@
+#%%
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -61,6 +62,11 @@ class rydberg_noisy_gate():
         self.K_array = K_array
         self.gamma = gamma
         
+    def __debugger(self, text, tbp, debug):
+        if debug == 2:
+            print(text, tbp)
+        return(None)
+    
     def single_qubit_gate_ryd(self):
         o_p, t, d, o, gam1, gam2, gamr = sp.symbols('o_p, t, d, o, gam1, gam2, gamr', real = True)
         x1, x2 = sp.symbols('x1, x2')
@@ -189,16 +195,16 @@ class rydberg_noisy_gate():
             tot =  sp.simplify(np.conj(v_m1).T @ expr1 @ np.conj(vec).T @ L[ind] @ vec @ expr2 @ v_m1 )
         
             mat = sp.flatten(tot)
-    
+
             corr_r = np.zeros([len(mat), len(mat)])
             corr_im = np.zeros([len(mat), len(mat)])
             
             for k in range(len(mat)):
                 for j in range(len(mat)):
-                    binary_1 = mat[k].is_zero
-                    binary_2 = mat[j].is_zero
+                    binary_1 = t in mat[k].free_symbols
+                    binary_2 = t in mat[j].free_symbols
                     
-                    if binary_1 != True and binary_2 != True:
+                    if binary_1 == True and binary_2 == True:
                         if k == j:
                             corr_r[k,k] = self.__variance(sp.re(mat[k]))
                         else:
@@ -206,10 +212,10 @@ class rydberg_noisy_gate():
             
             for k in range(len(mat)):
                 for j in range(len(mat)):
-                    binary_1 = mat[k].is_zero
-                    binary_2 = mat[j].is_zero
+                    binary_1 = t in mat[k].free_symbols
+                    binary_2 = t in mat[j].free_symbols
                     
-                    if binary_1 != True and binary_2 != True:
+                    if binary_1 == True and binary_2 == True:
                         if k == j:
                             corr_im[k,k] = self.__variance(sp.im(mat[k]))
                         else:
@@ -217,7 +223,10 @@ class rydberg_noisy_gate():
         
             corr_rs.append(corr_r)
             corr_ims.append(corr_im)
-            
+
+            self.__debugger('corr_rs',corr_rs, debug = 0)
+            self.__debugger('corr_ims',corr_ims, debug = 0)
+
         return (corr_rs, corr_ims)
     
     def __stoch_part(self, corr_rs, corr_ims):
@@ -245,7 +254,8 @@ class rydberg_noisy_gate():
             
             m += np.sqrt(self.gamma[ind])*result
 
-        
+        self.__debugger('stochpart',m, debug = 1)
+
         return(1J*m)
             
             

run.py
@@ -21,8 +21,8 @@ calling noisygate_rydberg_numerical
 
 psi_0 = np.zeros([4])
 psi_0[0] = 1
-N = 3000
-shots = 1
+N = 2000
+shots = 100
 t1 = 0.04 #amplitude damping #4s is in thesis
 
 t2 = 300*10**(-3) #dephasing damping
@@ -35,7 +35,7 @@ tg = np.pi/o_real
 gamma_1 = tg/t1
 gamma_2 = tg/t2
 
-gamma = [gamma_1, gamma_1]
+gamma = [gamma_1, gamma_1, gamma_1]
 
 K_array = [np.array(([0, 0, 0, 0],
 [0, 0, 0, 0],
@@ -44,25 +44,20 @@ K_array = [np.array(([0, 0, 0, 0],
            np.array(([0, 0, 0, 0],
 [0, 0, 0, 0],
 [0, 0, 0, 0], 
-[1, 0, 0, 0]))]
-
-'''dephasing
-
-gamma = [gamma_2]
-
-K_array = [np.array(([1, 0, 0, 0],
+[1, 0, 0, 0])),
+            np.array(([1, 0, 0, 0],
 [0, -1, 0, 0],
 [0, 0, 0, 0],
 [0, 0, 0, 0]))]
-'''
+
 o = np.pi
 d = 0
 
 tst = ng.rydberg_noisy_gate(K_array, o, d, 1, gamma)
 
-results = tst.singlequbit_sample_runs(psi_0, N, shots)
+results_num = tst.singlequbit_sample_runs(psi_0, N, shots)
 
-plt.plot(results, color = 'tab:red', label = 'noisygate')
+plt.plot(results_num, color = 'tab:red', label = 'noisygate')
 #plt.axvline(x=t1/tg, label='$T_{dp}$', color = 'black', linestyle='dashed', alpha = 0.7)
 # %%
 '''
@@ -101,22 +96,23 @@ tst = nrsa.single_noisy_gate(np.pi, 0, K_array, gamma)
 
 psi_0 = np.zeros([4])
 psi_0[0] = 1
-N = 3000
-shots = 1
+N = 2000
+shots = 100
 
 o = np.pi
 d = 0.00001 #d = 0 causes overflow warnings, might want to add an exception
 o_p = np.sqrt(o**2 + d**2)
 
-results = tst.singlequbit_sample_runs(psi_0, N, shots, params = [1, o_p, d, o, 1, 1] ) #t, o_p, d, o, x1, x2
+results_anly = tst.singlequbit_sample_runs(psi_0, N, shots, params = [1, o_p, d, o, 1, 1] ) #t, o_p, d, o, x1, x2
 #plt.title('Time-evolution of |0> state')
 plt.ylabel(r"$\rho_{00}$")
 plt.xlabel(r'time in [$t_g$]')
 
 #plt.axvline(x=t1/tg, label='$T_a$', color = 'orange', linestyle='dashed', alpha = 0.5)
 #plt.axvline(x=t2/tg, label='$T_{dp}$', color = 'black', linestyle='dashed', alpha = 0.7)
-plt.plot(results[0], color = 'tab:red', label = 'noisygate')
+plt.plot(results_anly[0], color = 'tab:red', label = 'noisygate')
 
 plt.legend()
 #plt.savefig('noisygate_rydberg_sq_1000shots_10khzdrive_00.pdf', dpi=1000, bbox_inches = 'tight')
 # %%
+plt.plot(np.abs(results_num - results_anly))